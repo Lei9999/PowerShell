45ab6bcdd2234a4812f09c47ab2eeaaab7747cc6
Lei9999 <shilei66491@126.com>
2021-05-10 09:57:14 +0800

    commit

--- Get-ADsth.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module ActiveDirectory
-
-Get-ADUser -Filter * -Properties pwdLastSet,PasswordLastset,EmailAddress | Where-Object Enable -Like true | Select-Object Name,SamAccountName,pwdLastSet,PasswordLastSet,EmailAddress | Format-Table
-# 查询账户"u1"的Propertices的所有值
-Get-ADUser u1 -Propertices *
